03805946476d0535e1700f136b0cdfbb5e705dbf
Yan <discxnnectedexehse@gmail.com>
2025-02-10 21:27:14 +0300

    Done

---
+++ Занятие 10 02/sss.py
@@ -0,0 +1,12 @@
+print("hello world")
+
+
+
+class Arsenii:
+    def __init__(self, age, name):
+        self.name = name
+        self.age = age
+    
+
+    def push(self):
+        print(self.name, self.age)
